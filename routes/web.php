b5ff480fa22bfba634131064f430f2618833b659
baazz20 <yaoeloge@outlook.fr>
2021-09-29 03:04:26 +0000

    intalation de tailwind css component et configuration du front terminer.

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('bienvenue');
-});
+Route::get('/',[PostController::class, 'index'])->name('posts.index');
+Route::resource('posts', PostController::class)->except('index');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
